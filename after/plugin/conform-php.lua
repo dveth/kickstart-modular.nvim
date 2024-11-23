33cf21c2b9abeaf49084594f7844e2d47a593e47
dveth <kanosauries@gmail.com>
2024-11-22 22:03:06 -0600

    fix conform, esp. for rust

--- after/plugin/conform-php.lua
+++ after/plugin/conform-php.lua
@@ -1,8 +1,13 @@
-require('conform').formatters.phpcbf = function()
-  return {
-    command = 'C:\\Users\\Terry\\AppData\\Roaming\\Composer\\vendor\\bin\\phpcbf',
-    prepend_args = { '--standard=WordPress' },
-    -- The base args are { "-filename", "$FILENAME" } so the final args will be
-    -- { "-i", "2", "-filename", "$FILENAME"
-  }
-end
+require('conform').formatters.phpcbf = {
+  command = 'C:\\Users\\Terry\\AppData\\Roaming\\Composer\\vendor\\bin\\phpcbf',
+  prepend_args = { '--standard=WordPress' },
+  -- The base args are { "-filename", "$FILENAME" } so the final args will be
+  -- { "-i", "2", "-filename", "$FILENAME" }
+}
+
+vim.api.nvim_create_autocmd('BufWritePre', {
+  pattern = '*',
+  callback = function(args)
+    require('conform').format { bufnr = args.buf }
+  end,
+})
--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -169,8 +169,13 @@ return {
       local servers = {
         -- clangd = {},
         gopls = {},
-        -- pyright = {},
-        -- rust_analyzer = {},
+        pyright = {},
+        rust_analyzer = {
+          checkOnSave = {
+            command = 'clippy',
+          },
+        },
+        bacon = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
