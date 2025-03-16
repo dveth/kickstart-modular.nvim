a40f297e507bcf2556f7983e15c371186f88f26d
dveth <kanosauries@gmail.com>
2025-03-16 11:56:34 -0500

    added keymaps for telescope

--- lua/kickstart/plugins/todo-comments.lua
+++ lua/kickstart/plugins/todo-comments.lua
@@ -1,5 +1,14 @@
 -- Highlight todo, notes, etc in comments
 return {
-  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
+  {
+    'folke/todo-comments.nvim',
+    event = 'VimEnter',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    opts = { signs = false },
+    config = function()
+      vim.keymap.set('n', '<leader>todo', ':TodoTelescope keywords=TODO,FIX<cr>')
+      require('todo-comments').setup()
+    end,
+  },
 }
 -- vim: ts=2 sts=2 sw=2 et
